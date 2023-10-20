161f910bbf19b00c0a02119df7fb8b059da25301
Vasjen <vasjenm@gmail.com>
2023-10-20 13:39:49 +0300

    Complete mobile navigation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navImg = document.querySelector('#nav-btn-img');
+navBtn.onclick = () => {
+   if  (nav.classList.toggle('open')){
+        navImg.src = './img/nav/nav_close.svg';
+    } else {
+        navImg.src = './img/nav/nav.svg';
+    }
+}
